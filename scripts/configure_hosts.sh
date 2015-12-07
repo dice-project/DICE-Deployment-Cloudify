d91199f2c9cf34a5078edd6e84a5c70510b5d852
Tadej Borovšak <tadej.borovsak@xlab.si>
2015-12-07 13:54:57 +0100

    Patch storm cluster /etc/hosts file
    We brute force insert complete set of mappings for fixed IP range. Not
    something we should be proud of.

--- scripts/configure_hosts.sh
+++ scripts/configure_hosts.sh
@@ -5,4 +5,11 @@ echo "127.0.1.1 $HOSTNAME" | sudo tee -a /etc/hosts
 
 ctx logger info "Configured chef server host name"
 
+for n in $(seq 255)
+do
+  echo "109.231.122.$n 109-231-122-$n" | sudo tee -a /etc/hosts
+done
+
+ctx logger info "Patched /etc/hosts file"
+
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsrH2o/WY+DLTJ/gxzIIV4slSkk0b+qQTteIDYidjcSdl0sp5HaE3UVhJ0xRP3LSVK4+SKC6LW/42N6/XzXX1V+O0CmAEVXxfhu97ZMbK1C8+iyo4bNVzabgI5pzUIAsh8c8WWBMgrey5O9MJkzDv8heZaauB+C1uw6G/uF5fFrhvYGVPokb1YaFKsq0cqkPG6usINByxPmgDV2LIHXkPKMktodyGFXmvk+Z9wWqVEzyzaQbnarXXaTd73LupFJAJBQdwm08LCasDs/sunSrr9m4KxDv+sKN0ybxZFjPIOrK2fUzMF1t6u4WTsdZ107G6u/KieEuMlchGVbJ3UDMsnQ== matej@virtubuntutej" >> ~/.ssh/authorized_keys
