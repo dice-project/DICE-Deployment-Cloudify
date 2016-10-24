728bad40892c677b46575b7c55faea414932ef39
Tadej Borovšak <tadej.borovsak@xlab.si>
2016-10-24 08:40:02 +0200

    Add Apache Storm monitoring support
    By default, monitoring is disabled, which should ensure backwards
    compatibility.
    
    Change-Id: I71b1e9fcfe7547f4c116fa256272e2857547ad88

--- dice_plugin/tasks/storm.py
+++ dice_plugin/tasks/storm.py
@@ -20,6 +20,7 @@
 #     Tadej Borovšak <tadej.borovsak@xlab.si>
 
 import os
+import json
 import yaml
 import requests
 import tempfile
@@ -73,6 +74,55 @@ def submit_topology(ctx, jar, name, klass, args):
         msg = "Topology '{}' failed to start properly".format(name)
         raise NonRecoverableError(msg)
     ctx.instance.runtime_properties["topology_id"] = topology_id
+
+
+@operation
+def register_topology(ctx, topology_id, monitoring):
+    if not monitoring["enabled"]:
+        ctx.logger.info("Monitoring is disabled. Skiping registration.")
+        return
+
+    dmon_tmpl = "http://{dmon_address}/dmon/v1/overlord/core/ls"
+    dmon_logstash_endpoint = dmon_tmpl.format(**monitoring)
+    nimbus_ip = ctx.instance.host_ip
+
+    msg = "Registering topology {} to dmon {}."
+    ctx.logger.info(msg.format(topology_id, dmon_logstash_endpoint))
+
+    ctx.logger.info("Checking if topology is actually running.")
+    topology_url = "http://{}:8080/api/v1/topology/{}".format(nimbus_ip,
+                                                              topology_id)
+    rst_json = requests.get(topology_url).json()
+    if (len(rst_json["bolts"]) == 0) and (len(rst_json["spouts"]) == 0):
+        ctx.operation.retry("Topology is not running yet")
+
+    ctx.logger.info("Getting the current configuration from dmon.")
+    r = requests.get(dmon_logstash_endpoint)
+    if r.status_code != 200:
+        msg = "Failed to obtain the configuration: {}\n{}"
+        raise NonRecoverableError(msg.format(r.status_code, r.text))
+
+    logstash_cfg = json.loads(r.text)["LS Instances"][0]
+    topology_cfg = {
+        "ESClusterName": logstash_cfg["ESClusterName"],
+        "HostFQDN": logstash_cfg["HostFQDN"],
+        "LSCoreStormTopology": topology_id,
+        "LSCoreStormPort": 8080,
+        "LSCoreStormEndpoint": nimbus_ip,
+    }
+
+    ctx.logger.info("Registering with: {}".format(json.dumps(topology_cfg)))
+    r = requests.put("{}/config".format(dmon_logstash_endpoint),
+                     json=topology_cfg)
+    ctx.logger.info("Response: {}".format(r.text))
+
+    if r.status_code != 201:
+        msg = "Failed to update logstash configuration: {}\n{}"
+        raise NonRecoverableError(msg.format(r.status_code, r.text))
+
+    ctx.logger.info("Requesting logstash restart.")
+    r = requests.post(dmon_logstash_endpoint)
+    ctx.logger.info("Response: {0}".format(r.text))
 
 
 @operation
