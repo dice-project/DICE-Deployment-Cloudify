b30ef502d626416634bc8ca5ba36e428309a2151
Tadej Borovšak <tadej.borovsak@xlab.si>
2016-10-25 12:46:38 +0200

    Add script runner TOSCA type
    This node type should be used to perform modifications of existing
    node instances. For examples, this can be used to connect services,
    defined in TOSCA blueprint, with some external ones. Another use would
    be to prepare message queues on broker.
    
    Change-Id: I2bfd99be8c6f0a7c13711dda56d512410660b0bc

---
+++ dice_plugin/tasks/misc.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 XLAB d.o.o.
+#
+# This file is part of dice-plugin.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the
+# License for the specific language governing permissions and * limitations
+# under the License.
+#
+# Author:
+#     Tadej Borovšak <tadej.borovsak@xlab.si>
+
+import os
+import subprocess
+
+from dice_plugin import utils
+from cloudify.decorators import operation
+from cloudify.exceptions import NonRecoverableError
+
+
+@operation
+def run_script(ctx, script, arguments, language):
+    supported_langs = {"bash"}
+
+    if language not in supported_langs:
+        msg = "Unknown language: {}. Available languages: {}."
+        raise NonRecoverableError(
+            msg.format(language, ", ".join(supported_langs))
+        )
+
+    ctx.logger.info("Getting '{}' script".format(script))
+    local_script = utils.obtain_resource(ctx, script)
+    cmd = map(str, [language, local_script] + arguments)
+
+    ctx.logger.info("Running command: {}".format(" ".join(cmd)))
+    proc = subprocess.Popen(cmd, stdin=open(os.devnull, "r"),
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in iter(proc.stdout.readline, ""):
+        ctx.logger.info(line.strip())
+    proc.wait()
+
+    if proc.returncode != 0:
+        msg = "Script terminated with non-zero ({}) status."
+        raise NonRecoverableError(msg.format(proc.returncode))
